2d8740a141f0ad52dd69223026854882422c0710
Dario Garcia <dario61081@gmail.com>
2021-09-27 19:04:51 -0400

    inicial

---
+++ PyHomer.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.jinja_env.cache = {}
+app.secret_key = os.urandom(12345)
+
+
+@app.context_processor
+def context_processor():
+    return dict(appname="PyHomer")
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv("PORT", 1234))
